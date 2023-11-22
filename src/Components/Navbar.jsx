b92108f635d070aa0eaea30325b4f11396a25bdd
yahyacodes <yahya.codin@gmail.com>
2023-11-22 23:26:57 +0300

    First commit of the post feed application

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-900">
+      <div className="max-w-screen-xl mx-auto px-4 py-3 text-white sm:text-center md:px-8">
+        <p className="font-medium">Post Feed</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
